6858d02399990c32902e9a4457481daad4c91cec
Nawanshu Choudhary <nawanshu@gmail.com>
2019-01-09 14:01:29 +0530

    Embed PHP inside HTML code

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
-<?php
-    $con = new mysqli('127.0.0.1', "root", 'passwordgit add ', 'mydb');
-    $message = $con->query("SELECT message FROM mytable")->fetch_object()->message;
-    $con->close();
-    echo "$message <br/>";
-    echo "This is my first PHP project. Basic database connection test run successful.";
-    phpinfo();
-?>
+<html>
+    <head>
+        <title>This is my PHP website</title>
+    </head>
+<body>
+    <center>
+        <h1>
+        <?php
+            //Setting up database
+            $con = new mysqli('127.0.0.1', "root", 'password', 'mydb');
+
+            /* Fetching data from database */
+            $message = $con->query("SELECT message FROM mytable")->fetch_object()->message;
+            $con->close();
+            echo "$message <br/>";
+            echo "This is my first PHP project. Basic database connection test run successful.";
+        ?>
+        </h1>
+    </center>
+</body>
+</html>
